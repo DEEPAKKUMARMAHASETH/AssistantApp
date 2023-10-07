2c38df2edeb65c73d528ff5e4092de901b110899
Deepak Mahaseth <deepakkumarmahaseth5@gmail.com>
2023-10-07 21:47:57 +0530

    coded

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,25 +1,39 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 const Contact = () => {
-    return (
-        <div class="container" style={{backgroundColor:"#b9bebd", marginTop:"40px" ,marginBottom:"40px"}}>
-            <h1 className="font-weight-bold text-center ">CONTACT US</h1>
-            <form  action="https://formsubmit.co/el/difiwa" method="POST">
-                <div class="form-group">
-                    <div class="form-row">
-                        <div class="col">
-                            <input type="text" name="name" class="form-control" placeholder="Full Name" required />
-                        </div>
-                        <div class="col">
-                            <input type="email" name="email" class="form-control" placeholder="Email Address" required />
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_u1c4qer', 'template_vd7eig5', form.current, 'j0kw8sFwbByiq61QU')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+        return (
+            <div class="container" style={{ backgroundColor: "#b9bebd", marginTop: "40px", marginBottom: "40px" }}>
+                <h1 className="font-weight-bold text-center ">CONTACT US</h1>
+                <form ref={form} onSubmit={sendEmail}>
+                    <div class="form-group">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="text" name="name" class="form-control" placeholder="Full Name" required />
+                            </div>
+                            <div class="col">
+                                <input type="email" name="email" class="form-control" placeholder="Email Address" required />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
-                </div>
-                <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
-            </form>
-        </div>
-    )
-}
+                    <div class="form-group">
+                        <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+                </form>
+            </div>
+        )
+    }
 
-export default Contact
+    export default Contact
